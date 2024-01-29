4f249a83aef69c8ed45b391c6b9fb7a1e1530acd
Lilith Song <lsong@princessrtfm.com>
2024-01-28 19:25:29 -0500

    refactoring shit again
    can you tell I'm getting more tired as these commits go on

--- WoLua/Lua/Docs/SkipDocAttribute.cs
+++ WoLua/Lua/Docs/SkipDocAttribute.cs
@@ -4,7 +4,5 @@ namespace PrincessRTFM.WoLua.Lua.Docs;
 
 [AttributeUsage(AttributeTargets.Property | AttributeTargets.Method)]
 internal class SkipDocAttribute: Attribute {
-	public SkipDocAttribute(string justification) {
-		_ = justification;
-	}
+	public SkipDocAttribute(string justification) => _ = justification;
 }
--- WoLua/Lua/LuaGlobalAttribute.cs
+++ WoLua/Lua/LuaGlobalAttribute.cs
@@ -5,7 +5,5 @@ namespace PrincessRTFM.WoLua.Lua;
 [AttributeUsage(AttributeTargets.Property)]
 internal class LuaGlobalAttribute: Attribute {
 	public readonly string Name;
-	public LuaGlobalAttribute(string name) {
-		this.Name = name;
-	}
+	public LuaGlobalAttribute(string name) => this.Name = name;
 }
